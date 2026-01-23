cddfc0e9fe72cbd1413f711ed475fff1efc54962
geethika <tvgeethika02@gmail.com>
2026-01-23 10:38:01 +0530

    Created a portfolio

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+var a = 10;
+console.log(a);
+let b=20;
+console.log(b);
+const c=30;
+console.log(c);
+
+let i =0;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+i =8;
+let res=(i%2==0)?console.log("even"):console.log("odd");
+var add = ()=>{
+    console.log(10+20);
+}
+add();
